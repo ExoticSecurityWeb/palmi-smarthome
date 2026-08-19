516306fff4dc59e3a264b797a0731a4dfd983a3c
ExoticSecurityWeb <pinkparadiiise@gmail.com>
2026-08-20 01:50:54 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -3,20 +3,14 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.DASHBOARD_PORT || 3001;
+const PORT = 3002;
 
-// Fichiers du dashboard
 app.use(express.static(__dirname));
 
-// Page principale
 app.get("/", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "dashboard.html")
-    );
+    res.sendFile(path.join(__dirname, "dashboard.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(
-        `🌴 Palmi Dashboard lancé sur le port ${PORT}`
-    );
+    console.log(`🌴 Palmi Dashboard lancé sur le port ${PORT}`);
 });
